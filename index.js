cd22c626f6f74b08f18a384fbe63630eb6f3bcf3
shloakaduvvuri <90352911+shloakaduvvuri@users.noreply.github.com>
2021-09-08 15:22:49 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,97 +1,43 @@
-import ReactDOM from 'react-dom';
-import React, { Component } from 'react';
-import { w3cwebsocket as W3CWebSocket } from "websocket";
-import { Card, Avatar, Input, Typography } from 'antd';
-import 'antd/dist/antd.css';
-import './index.css'
+const webSocketsServerPort = 8000;
+const webSocketServer = require('websocket').server;
+const http = require('http');
 
-const { Search } = Input;
-const { Text } = Typography;
-const { Meta } = Card;
+// Spinning the http server and the websocket server.
+const server = http.createServer();
+server.listen(webSocketsServerPort);
+console.log('listening on port 8000');
 
-const client = new W3CWebSocket('ws://127.0.0.1:8000');
 
-export default class App extends Component {
+const wsServer = new webSocketServer({
+  httpServer: server
+});
 
-  state ={
-    userName: '',
-    isLoggedIn: false,
-    messages: []
-  }
+const clients = {};
 
-  onButtonClicked = (value) => {
-    client.send(JSON.stringify({
-      type: "message",
-      msg: value,
-      user: this.state.userName
-    }));
-    this.setState({ searchVal: '' })
-  }
-  componentDidMount() {
-    client.onopen = () => {
-      console.log('WebSocket Client Connected');
-    };
-    client.onmessage = (message) => {
-      const dataFromServer = JSON.parse(message.data);
-      console.log('got reply! ', dataFromServer);
-      if (dataFromServer.type === "message") {
-        this.setState((state) =>
-          ({
-            messages: [...state.messages,
-            {
-              msg: dataFromServer.msg,
-              user: dataFromServer.user
-            }]
-          })
-        );
+// This code generates unique userid for everyuser.
+const getUniqueID = () => {
+  const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  return s4() + s4() + '-' + s4();
+};
+
+wsServer.on('request', function (request) {
+  var userID = getUniqueID();
+  console.log((new Date()) + ' Recieved a new connection from origin ' + request.origin + '.');
+
+  // You can rewrite this part of the code to accept only the requests from allowed origin
+  const connection = request.accept(null, request.origin);
+  clients[userID] = connection;
+  console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients));
+
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+      console.log('Received Message: ', message.utf8Data);
+
+      // broadcasting message to all connected clients
+      for(key in clients) {
+        clients[key].sendUTF(message.utf8Data);
+        console.log('sent Message to: ', clients[key]);
       }
-    };
-  }
-  render() {
-    return (
-      <div className="main">
-        {this.state.isLoggedIn ?
-        <div>
-          <div className="title">
-            <Text type="secondary" style={{ fontSize: '36px' }}>Websocket Chat</Text>
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'column', paddingBottom: 50 }}>
-            {this.state.messages.map(message => 
-              <Card key={message.msg} style={{ width: 300, margin: '16px 4px 0 4px', alignSelf: this.state.userName === message.user ? 'flex-end' : 'flex-start' }} loading={false}>
-                <Meta
-                  avatar={
-                    <Avatar style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>{message.user[0].toUpperCase()}</Avatar>
-                  }
-                  title={message.user}
-                  description={message.msg}
-                />
-              </Card> 
-            )}
-          </div>
-          <div className="bottom">
-            <Search
-              placeholder="input message and send"
-              enterButton="Send"
-              value={this.state.searchVal}
-              size="large"
-              onChange={(e) => this.setState({ searchVal: e.target.value })}
-              onSearch={value => this.onButtonClicked(value)}
-            />
-          </div> 
-        </div>
-        :
-        <div style={{ padding: '200px 40px' }}>
-          <Search
-            placeholder="Enter Username"
-            enterButton="Login"
-            size="large"
-            onSearch={value => this.setState({ isLoggedIn: true, userName: value })}
-          />
-        </div>
-      }
-      </div>
-    )
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+    }
+  })
+});
